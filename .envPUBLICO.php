df490cba3ed2263618c33c74dfcd9721ebb2cdf6
AdanVolken <rolfivolken9@gmail.com>
2021-10-19 19:44:18 -0300

    Primer commit pc2

---
+++ .envPUBLICO.php
@@ -0,0 +1,9 @@
+<?php
+function credenciales() {
+    return [
+        'usuario'=>'Adan' ,
+        'clave'=>'adanvolken',
+        'servidor'=>'localhost',
+        'base_de_datos'=>'banco'
+    ];
+}
